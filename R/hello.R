8c063d5876828a1fceca0e0fac0c856296684bb6
Dominic Maderazo <dmaderazo@student.unimelb.edu.au>
2018-11-13 16:09:06 +1100

    initial commit

---
+++ R/hello.R
@@ -0,0 +1,54 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+
+hello <- function() {
+  print("Hello, world!")
+}
+
+
+#' Goodbye
+#'
+#' This is a function
+#'
+#' yeet
+#' @return A vector of elements drawn from \code{x}.
+#'
+#' @examples
+goodbye <- function(){
+  print("Goodbye")
+}
+
+#' Square
+#'
+#' asdf
+#'
+#' @return numeric vector
+#' @export
+#'
+#' @examples
+square <- function(x){
+  x^2
+}
+
+#' Sample code
+#'
+#' @return
+#' @export
+#'
+#' @examples
+package_test <- function(){
+  print("This is now working")
+}
